ad5377bbbba769dbedf3fb06bd9f9eeb57900d2b
devan1011 <dan.e@teemill.com>
2022-03-16 21:01:53 +0000

    removes build folder

--- build/src/nodeType.d.ts
+++
@@ -1,9 +0,0 @@
-import { FlowIODef, FlowIOSet } from './io';
-export declare type FlowNodeHandler<I, O> = (input: <K extends keyof I>(key: K) => I[K], output: <K extends keyof O>(key: K, value: O[K]) => void) => void;
-export declare class FlowNodeType<I extends FlowIOSet, O extends FlowIOSet> {
-    name: string;
-    inputs: FlowIODef<I>;
-    outputs: FlowIODef<O>;
-    handler: FlowNodeHandler<I, O>;
-    constructor(name: string, inputs: FlowIODef<I>, outputs: FlowIODef<O>, handler: FlowNodeHandler<I, O>);
-}
